a2c63de95b7c1a6130fc74f90ca796b16a5cd64a
Your Name <samadhinilakshana1999@gmail.com>
2022-09-16 22:04:51 +0530

    Update Content

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
+import Posts from "../../posts/Posts";
+import Sidebar from "../../sidebar/Sidebar"
 import Header from "../../header/Header"
 import React from 'react'
 import "./home.css";
 
 export default function Home() {
   return (
+    <>
+    <Header/>
     <div className="home">
-        <Header/>
-        homePage
+      <Posts/>
+      <Sidebar/>
     </div>
+    </>
   )
 }
