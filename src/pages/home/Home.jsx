81698515bc8b5d92d88b355cab3b80f713dde9bc
Your Name <samadhinilakshana1999@gmail.com>
2022-09-16 13:54:13 +0530

    Update Content

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../../header/Header"
+import React from 'react'
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+        <Header/>
+        homePage
+    </div>
+  )
+}
