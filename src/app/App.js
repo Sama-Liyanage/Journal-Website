756ad69d6e7ce9e31119d8347be82a69367008ac
Your Name <samadhinilakshana1999@gmail.com>
2022-09-17 23:43:45 +0530

    Update Content

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,12 @@
 import Home from "../pages/home/Home";
 import TopBar from "../components/topbar/TopBar";
-import Single from "../pages/home/single/Single";
+import Write from "../pages/home/write/Write";
 
 function App() {
   return (
     <>
     <TopBar/>
-    <Single/>
+    <Write/>
     </>
   );
 }
