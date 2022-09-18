98e0c62d12b83960ff3eec4b901ea2c05f471632
Your Name <samadhinilakshana1999@gmail.com>
2022-09-18 14:56:07 +0530

    Update Content

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -3,27 +3,27 @@ import profileImage from "../../assets/profileImage.jpg"
 
 //rfc
 export default function TopBar() {
-  return (
-    <div className="top">
-        <div className='topLeft'>
-            <i className="topIcon fa-brands fa-facebook"></i>
-            <i className="topIcon fa-brands fa-pinterest"></i>
-            <i className="topIcon fa-solid fa-envelope-circle-check"></i>
-        </div>
+    return (
+        <div className="top">
+            <div className='topLeft'>
+                <i className="topIcon fa-brands fa-facebook"></i>
+                <i className="topIcon fa-brands fa-pinterest"></i>
+                <i className="topIcon fa-solid fa-envelope-circle-check"></i>
+            </div>
 
-        <div className='topCenter'>
-            <ul className="topList">
-                <li className="topListItem">HOME</li>
-                <li className="topListItem">ABOUT</li>
-                <li className="topListItem">CONTACT</li>
-                <li className="topListItem">WRITE</li>
-                <li className="topListItem">LOGOUT</li>
-            </ul>
+            <div className='topCenter'>
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className='topRight'>
+                <img className="topImg" src={profileImage}></img>
+                <i className=" topSearchIcon fa-solid fa-magnifying-glass"></i>
+            </div>
         </div>
-        <div className='topRight'>
-            <img className="topImg" src={profileImage}></img>
-            <i className=" topSearchIcon fa-solid fa-magnifying-glass"></i>
-        </div>
-    </div>
-  )
+    )
 }
