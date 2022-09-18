abfca54b36f64e471c842708f26d9b80e3d3e412
Your Name <samadhinilakshana1999@gmail.com>
2022-09-18 14:54:26 +0530

    Update Content

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -8,38 +8,38 @@ export default function SinglePost() {
       <div className="singlePostWrapper">
         <img src={singlePostImg} alt="" className="singlePostImg" />
         <h1 className="singlePostTitle">
-            Lorem ipsum dolor sit amet consectetur
-            <div className="singlePostEdit">
-                <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
-                <i className="singlePostIcon fa-solid fa-trash"></i>
-            </div>    
+          Lorem ipsum dolor sit amet consectetur
+          <div className="singlePostEdit">
+            <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
+            <i className="singlePostIcon fa-solid fa-trash"></i>
+          </div>
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">Author: <b>Samadhi Nilakshana</b></span>
           <span className="singlePostDate">1 hour ago</span>
           <p className="singlePostDesc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Pariatur architecto, reiciendis numquam, inventore aut itaque
             dolores commodi nihil porro laborum illum dolor, non culpa
-            dolorum voluptatum ad at excepturi odio. Lorem ipsum dolor 
-            sit amet, consectetur adipisicing elit. Deserunt quo modi 
-            asperiores, libero molestias ipsum recusandae possimus quos, 
-            iusto incidunt tenetur illo perferendis, similique qui 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Pariatur architecto, reiciendis numquam, inventore aut itaque
-            dolores commodi nihil porro laborum illum dolor, non culpa
-            dolorum voluptatum ad at excepturi odio. Lorem ipsum dolor 
-            sit amet, consectetur adipisicing elit. Deserunt quo modi 
-            asperiores, libero molestias ipsum recusandae possimus quos, 
+            dolorum voluptatum ad at excepturi odio. Lorem ipsum dolor
+            sit amet, consectetur adipisicing elit. Deserunt quo modi
+            asperiores, libero molestias ipsum recusandae possimus quos,
             iusto incidunt tenetur illo perferendis, similique qui
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Pariatur architecto, reiciendis numquam, inventore aut itaque
             dolores commodi nihil porro laborum illum dolor, non culpa
-            dolorum voluptatum ad at excepturi odio. Lorem ipsum dolor 
-            sit amet, consectetur adipisicing elit. Deserunt quo modi 
-            asperiores, libero molestias ipsum recusandae possimus quos, 
-            iusto incidunt tenetur illo perferendis, similique qui  
-             </p>
+            dolorum voluptatum ad at excepturi odio. Lorem ipsum dolor
+            sit amet, consectetur adipisicing elit. Deserunt quo modi
+            asperiores, libero molestias ipsum recusandae possimus quos,
+            iusto incidunt tenetur illo perferendis, similique qui
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Pariatur architecto, reiciendis numquam, inventore aut itaque
+            dolores commodi nihil porro laborum illum dolor, non culpa
+            dolorum voluptatum ad at excepturi odio. Lorem ipsum dolor
+            sit amet, consectetur adipisicing elit. Deserunt quo modi
+            asperiores, libero molestias ipsum recusandae possimus quos,
+            iusto incidunt tenetur illo perferendis, similique qui
+          </p>
         </div>
       </div>
     </div>
