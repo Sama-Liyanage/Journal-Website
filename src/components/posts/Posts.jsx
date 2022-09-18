04ca51d99963059e386e617287202cd2292fd73f
Your Name <samadhinilakshana1999@gmail.com>
2022-09-18 14:52:42 +0530

    Update Content

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -6,10 +6,10 @@ import Post from '../post/Post';
 export default function Posts() {
   return (
     <div className="posts">
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   )
 }
