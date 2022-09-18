ad7b461993ac7406a5b0c3bbfdc4cea503192857
Your Name <samadhinilakshana1999@gmail.com>
2022-09-18 14:53:37 +0530

    Update Content

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,37 +5,37 @@ import aboutMe from "../../assets/aboutMe.jpg"
 export default function sideBar() {
   return (
     <div className="sidebar">
-            <div className="sidebarItem">
-                <span className="sidebarTitle">
-                    ABOUT ME
-                </span>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">
+          ABOUT ME
+        </span>
 
-                <img src={aboutMe} alt="" />
+        <img src={aboutMe} alt="" />
 
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                   Quis perferendis quos animi velit iste unde provident eaque.</p>
-            </div>
-            <div className="sidebarItem">
-            <span className="sidebarTitle">CATEGORIES</span>
-            <ul className="sidebarList">
-                <li className="sidebarListItem">Life</li>
-                <li className="sidebarListItem">Music</li>
-                <li className="sidebarListItem">Style</li>
-                <li className="sidebarListItem">Sport</li>
-                <li className="sidebarListItem">Tech</li>
-                <li className="sidebarListItem">Cinema</li>
-            </ul>
-            </div>
-            <div className="sidebarItem ">
-                <span className="sidebarTitle">Follow Us</span>
-                <div className="sidebarSocial">
-                    <i className="sidebarIcon fa-brands fa-facebook"></i>
-                    <i className="sidebarIcon fa-brands fa-pinterest"></i>
-                    <i className="sidebarIcon fa-solid fa-envelope-circle-check"></i>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Quis perferendis quos animi velit iste unde provident eaque.</p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem ">
+        <span className="sidebarTitle">Follow Us</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-pinterest"></i>
+          <i className="sidebarIcon fa-solid fa-envelope-circle-check"></i>
 
-            </div>
+        </div>
 
-            </div>
+      </div>
     </div>
   )
 }
