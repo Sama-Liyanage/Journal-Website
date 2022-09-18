e765bec121dce12eace9f8202af8dc8a452c15d4
Your Name <samadhinilakshana1999@gmail.com>
2022-09-18 13:27:18 +0530

    Update Content

--- src/pages/home/write/Write.jsx
+++ src/pages/home/write/Write.jsx
@@ -4,9 +4,15 @@ import "./write.css";
 export default function Write() {
   return (
     <div className="write">
-        <form className="writeFormGroup">
-          
-        </form>
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i class="fa-light fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
+        </div>
+      </form>
     </div>
   )
 }
