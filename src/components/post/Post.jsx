cde4ffe82650240e5666b4fe3c6d19dc4e641d20
Your Name <samadhinilakshana1999@gmail.com>
2022-09-18 14:50:59 +0530

    Update Content

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,29 +5,29 @@ import postImg from "../../assets/postImg.jpg"
 export default function Post() {
   return (
     <div className="post">
-    <img className="postImg" src={postImg} alt="" />
-    <div className="postInfo">
-      <div className="postCats">
+      <img className="postImg" src={postImg} alt="" />
+      <div className="postInfo">
+        <div className="postCats">
           <span className="postCat">Music</span>
           <span className="postCat">Life</span>
-      </div>
-      <span className="postTitle">
+        </div>
+        <span className="postTitle">
           Lorem ipsum dolor, sit amet consectetur
-      </span>
-      <hr/>
-      <span className="postDate">1 hour ago</span>
-    </div>
-    <p className="postDesc">
+        </span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDesc">
         Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Expedita, sunt! Laudantium 
+        adipisicing elit. Expedita, sunt! Laudantium
         dolore ex commodi. Nisi voluptatum quasi commodi
         inventore ab, omnis, nemo expedita soluta maxime totam
-        similique, cupiditate minima fugit.Lorem, ipsum dolor 
+        similique, cupiditate minima fugit.Lorem, ipsum dolor
         sit amet consectetur adipisicing elit. Fugiat rerum,
         est omnis eligendi laboriosam consequatur! Illum laborum
         minus error repudiandae dicta ut laboriosam obcaecati
         amet! Molestias obcaecati quaerat porro reprehenderit?
-       </p>
-</div>
+      </p>
+    </div>
   )
 }
