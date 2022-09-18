56a1f6a04bbd8c65682868d0691eb9f25e5e509f
Your Name <samadhinilakshana1999@gmail.com>
2022-09-18 14:49:32 +0530

    Update Content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,12 +5,12 @@ import backgroundImg from "../../assets/backgroundImg.png"
 export default function Header() {
   return (
     <div className="header">
-            <div className="headerTitles">
-              <span className="headerTitleSm">React & Node</span>
-              <span className="headerTitleLg">Blog</span>
-            </div>
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
 
-            <img className="headerImg" src={backgroundImg} alt="" />
+      <img className="headerImg" src={backgroundImg} alt="" />
     </div>
   )
 }
