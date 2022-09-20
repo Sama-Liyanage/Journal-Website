b2875b85b52c634e28f57cb5719c8eb487634f62
Your Name <samadhinilakshana1999@gmail.com>
2022-09-20 22:52:44 +0530

    Complete RegisterPage

--- src/app/App.js
+++ src/app/App.js
@@ -2,13 +2,14 @@ import Home from "../pages/home/Home";
 import TopBar from "../components/topbar/TopBar";
 import Settings from "../pages/settings/Settings";
 import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
 
 
 function App() {
   return (
     <>
     <TopBar/>
-    <Login/>
+    <Register/>
     </>
   );
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./register.css";
+
+export default function Register() {
+    return (
+        <div className="register">
+            <span className="registerTitle">Register</span>
+            <form className="registerForm">
+                <label>User Name</label>
+                <input type="text" className="registerInput" placeholder="Enter your User Name" />
+                <label>E mail</label>
+                <input type="text" className="registerInput" placeholder="Enter your Email" />
+                <label>Password</label>
+                <input type="password" className="registerInput" placeholder="Enter your password" />
+
+                <button className="registerButton">Register</button>
+            </form>
+            <button className="registerLoginButton">Login</button>
+        </div>
+    )
+}
