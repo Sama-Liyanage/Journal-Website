dfd075dfd72db8bf8c4fd91b2bd8775caabeda0f
Your Name <samadhinilakshana1999@gmail.com>
2022-09-21 23:28:45 +0530

    Handle Routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,27 @@
-import Home from "../pages/home/Home";
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import TopBar from "../components/topbar/TopBar";
-import Settings from "../pages/settings/Settings";
-import Login from "../pages/login/Login";
+import Home from "../pages/home/Home";
 import Register from "../pages/register/Register";
+import Login from "../pages/login/Login";
+import Write from "../pages/home/write/Write"
+import Settings from "../pages/settings/Settings";
+import Single from "../pages/home/single/Single"
+
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
     <TopBar/>
-    <Register/>
-    </>
+    <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/register' element={<Register/>}/>
+        <Route exact path='/login' element={<Login/>}/>
+        <Route exact path='/write' element={<Write/>}/>
+        <Route exact path='/settings' element={<Settings/>}/>
+        <Route exact path='/post/:postId' element={<Single/>}/>
+    </Routes>
+</BrowserRouter>
   );
 }
 
